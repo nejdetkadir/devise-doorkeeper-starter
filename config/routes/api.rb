93fd109e812715061cec8b0fd9ed7c88cc3a5b6c
nejdetkadir <nejdetkadir.550@gmail.com>
2022-01-23 18:33:06 +0300

    Add swagger documentation

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,12 +2,6 @@
 
 namespace :api do
   namespace :v1 do
-    # Swagger documentation
-    scope :swagger do
-      get '/', to: 'apidocs#index', as: 'swagger_root'
-      get '/data', to: 'apidocs#data', as: 'swagger_data'
-    end
-
     scope :users, module: :users do
       post '/', to: 'registrations#create', as: :user_registration
     end
@@ -17,6 +11,12 @@ end
 # Doorkeeper integration
 scope :api do
   scope :v1 do
+    # Swagger documentation
+    scope :swagger do
+      get '/', to: 'apidocs#index', as: :swagger_root
+      get '/data', to: 'apidocs#data', as: :swagger_data
+    end
+
     use_doorkeeper do
       skip_controllers :authorizations, :applications, :authorized_applications
     end
