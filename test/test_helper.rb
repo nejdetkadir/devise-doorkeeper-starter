7112c4a77583741ad450ab2b18f3716354793152
nejdetkadir <nejdetkadir.550@gmail.com>
2022-01-23 16:25:27 +0300

    Add tests of login/logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "helpers/doorkeeper_params"
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
+  include Helpers::DoorkeeperParams
 
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
